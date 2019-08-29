5f31a7bb520afd8c79224b5a663e7bac210175a5
Yang Zhang <youngzh@umich.edu>
2019-08-29 17:13:46 -0400

    Include both math3 and math3-dev package

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
 wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install libignition-math3-dev libsdformat5 gazebo8 libgazebo8* ros-kinetic-gazebo8-* ros-kinetic-octomap ros-kinetic-octomap-msgs ros-kinetic-costmap-2d ros-kinetic-costmap-2d ros-kinetic-grid-map-ros
+sudo apt-get install libignition-math3* libsdformat5 gazebo8 libgazebo8* ros-kinetic-gazebo8-* ros-kinetic-octomap ros-kinetic-octomap-msgs ros-kinetic-costmap-2d ros-kinetic-costmap-2d ros-kinetic-grid-map-ros
 git submodule update --init --recursive
 cd src/swarm-primitive-dp/
 mkdir generated_launch
