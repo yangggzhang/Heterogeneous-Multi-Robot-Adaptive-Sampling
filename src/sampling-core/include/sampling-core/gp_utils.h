75a1b2c1bc3c0961de56f83a417d478643e462a0
Yang Zhang <youngzh@umich.edu>
2019-09-08 23:28:39 -0400

    stage for furture work

--- src/sampling-core/include/sampling-core/gp_utils.h
+++ src/sampling-core/include/sampling-core/gp_utils.h
@@ -8,7 +8,6 @@
 #define JC_VORONOI_IMPLEMENTATION
 #include "jc_voronoi.h"
 
-#include <CentralizedController.h>
 #include <cmuswarm_msgs/BehaviourRequest.h>
 #include <cmuswarm_msgs/ObjectCoverageLocation.h>
 #include <cmuswarm_msgs/ObjectCoverageLocations.h>
@@ -21,6 +20,28 @@
 #include "load_data.h"
 
 namespace sampling {
+
+const DefaultMaxMapSize = 400;
+
+struct EigenHash {
+    int operator()(const Eigen::MatrixXd &m)const {
+        int row = m(0,0);
+        int col = m(0,1);
+        int hash = row*DefaultMaxMapSize + col; 
+        return hash;
+    }
+};
+
+struct GroundTruthData{
+/*
+ * Fss represents temperature (945 x 1), Xss represents coordination (945 x 2)
+ */
+    Eigen::MatrixXd Xss;
+    Eigen::MatrixXd Fss;
+    int num_gau;
+    int num_bot;
+    unordered_map<Eigen::MatrixXd,double,EigenHash> gtSamples;
+};
 
 /// Inuput location_1 location_2
 /// Output distance between sets of locations
@@ -38,199 +59,32 @@ bool closest_index(const Eigen::MatrixXd &robots_location,
                    const Eigen::MatrixXd &cell_location, const int &robot_id,
                    std::vector<size_t> &closest_cell);
 
-Eigen::MatrixXd extract_rows(const Eigen::MatrixXd &X, const vector<int> &ind) {
-  Eigen::MatrixXd X_train(ind.size(), X.cols());
-
-  for (int i = 0; i < ind.size(); i++) {
-    X_train.row(i) = X.row(ind[i]);
-  }
-  return X_train;
-}
+Eigen::MatrixXd extract_rows(const Eigen::MatrixXd &X, const vector<int> &ind);
 
 Eigen::MatrixXd extract_rows(const Eigen::MatrixXd &X,
-                             Eigen::VectorXi &ind_train) {
-  Eigen::MatrixXd X_train(ind_train.size(), X.cols());
-
-  for (int i = 0; i < ind_train.size(); i++) {
-    X_train.row(i) = X.row(ind_train(i));
-  }
-  return X_train;
-}
+                             Eigen::VectorXi &ind_train);
 
 vector<int> powercellidx(const Eigen::MatrixXd &robot_positions,
-                         const Eigen::MatrixXd &Xss, const int &robot_id) {
-  Eigen::MatrixXd Distance = pdist2(robot_positions, Xss);  // 945 x 3
-  // cout<<"finish distance"<<endl;
-  // cout<<"distance size"<<Distance.rows() <<"x"<< Distance.cols()<<endl;
-  vector<int> q;
-  Eigen::MatrixXd::Index min_index;
-  for (int i = 0; i < Xss.rows(); i++) {
-    Distance.col(i).minCoeff(&min_index);
-    if (min_index == robot_id) q.push_back(i);
-  }
-  return q;
-}
+                         const Eigen::MatrixXd &Xss, const int &robot_id);
 
 vector<vector<int>> batch_powercellidx(const Eigen::MatrixXd &robot_positions,
-                                       const Eigen::MatrixXd &Xss) {
-  /*
-   * Input:
-   *      g: 3 x 2   robot locations
-   *      s: 945 x 2 Xss
-   * Output:
-   *      idx: 945 x 1
-   */
-  // cout<<"Robot positions "<<robot_positions.rows()<<"
-  // "<<robot_positions.cols()<<endl;
-  // cout<<"Xss "<<Xss.rows()<<" "<<Xss.cols()<<endl;
-  int num_robots = robot_positions.rows();
-
-  Eigen::MatrixXd Distance = pdist2(robot_positions, Xss);  // 945 x 3
-  // cout<<"finish distance"<<endl;
-  // cout<<"distance size"<<Distance.rows() <<"x"<< Distance.cols()<<endl;
-  vector<vector<int>> ans;
-  ans.resize(num_robots);
-  Eigen::MatrixXd::Index min_index;
-  for (int i = 0; i < Xss.rows(); i++) {
-    Distance.col(i).minCoeff(&min_index);
-    ans[min_index].push_back(i);
-  }
-  return ans;
-}
+                                       const Eigen::MatrixXd &Xss);
 
 Eigen::MatrixXd compute_utility_center(const int &robot_id,
                                        const Eigen::MatrixXd &rob_positions,
                                        const Eigen::MatrixXd &Xss,
-                                       const Eigen::MatrixXd &phi) {
-  // first get closest positions
-  std::cout << "Enter Computer Utility Center" << std::endl;
-  vector<int> q_index = powercellidx(rob_positions, Xss, robot_id);
-
-  std::cout << "Finish powercell" << std::endl;
-  // calculate voroni centers
-  double c_qx = 0.0;
-  double c_qy = 0.0;
-  double c_q = 0.0;
-
-  for (int i = 0; i < q_index.size(); i++) {
-    c_qx += Xss(q_index[i], 0) * phi(q_index[i]);
-    c_qy += Xss(q_index[i], 1) * phi(q_index[i]);
-    c_q += phi(q_index[i]);
-  }
-
-  Eigen::MatrixXd v_center = Eigen::MatrixXd::Zero(1, 2);
-  v_center(0, 0) = c_qx / c_q;
-  v_center(0, 1) = c_qy / c_q;
-
-  return v_center;
-}
+                                       const Eigen::MatrixXd &phi);
 
 Eigen::MatrixXd loggausspdf(Eigen::MatrixXd X, Eigen::MatrixXd mu,
-                            Eigen::MatrixXd Sigma) {
-  int d = X.rows();
-  int n = X.cols();
-  for (int i = 0; i < d; i++) {
-    for (int j = 0; j < n; j++) {
-      X(i, j) = X(i, j) - mu(i, 0);  // mu: d x 1
-    }
-  }
-  Eigen::MatrixXd U = Sigma.llt().matrixL();
-  Eigen::MatrixXd Q = U.inverse() * X;
-  Eigen::MatrixXd q = Q.array() * Q.array();
-
-  double c = d * log(2 * M_PI) + 2 * U.diagonal().array().log().sum();
-
-  Eigen::MatrixXd y = -1 * (c + q.array()) / 2;
-  return y.transpose();
-}
+                            Eigen::MatrixXd Sigma);
 
 double expectation(const Eigen::MatrixXd &X, Model &gpModel,
-                   Eigen::MatrixXd &R) {
-  // cout<<"before: "<<R<<endl;
-  Eigen::MatrixXd mu = gpModel.mu;
-  Eigen::MatrixXd Sigma = gpModel.Sigma;
-  Eigen::MatrixXd w = gpModel.w;
-
-  int d = X.rows();
-  int n = X.cols();
-  int k = mu.cols();
-
-  for (int i = 0; i < k; i++) {
-    R.col(i) = loggausspdf(X, mu.col(i), Sigma.block<1, 1>(0, i));
-  }
-  // cout<<"after 1: "<<R<<endl;
-
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < k; j++) {
-      R(i, j) = R(i, j) + w.array().log()(0, j);  // w: 1 x k
-    }
-  }
-  // cout<<"after 2: "<<R<<endl;
-
-  Eigen::MatrixXd T = R.array().exp().rowwise().sum().log();  // T: n x 1
-  double llh_iter = T.sum() / n;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < k; j++) {
-      R(i, j) = R(i, j) - T(i, 0);  // w: 1 x k
-    }
-  }
-  // cout<<"after 3: "<<R<<endl;
-
-  R = R.array().exp();
-  // cout<<"after: "<<R<<endl;
-
-  return llh_iter;
-}
+                   Eigen::MatrixXd &R);
 
 void maximization(const Eigen::MatrixXd &X, Model &gpModel,
-                  const Eigen::MatrixXd &R) {
-  int d = X.rows();             // d : utility dimension
-  int n = X.cols();             // n : number of samples
-  int k = gpModel.numGaussian;  // k : number of gaussian
+                  const Eigen::MatrixXd &R);
 
-  Eigen::MatrixXd nk = R.colwise().sum();
-  Eigen::MatrixXd w = nk / n;
-  Eigen::MatrixXd temp = X * R;
-  Eigen::MatrixXd mu(d, k);
-
-  for (int i = 0; i < d; i++) {
-    for (int j = 0; j < k; j++) {
-      mu(j) = temp(i, j) / nk(i, j);
-    }
-  }
-
-  Eigen::MatrixXd Sigma =
-      Eigen::MatrixXd::Zero(d, d * k);  // use block to construct a 3D matrix,
-                                        // leftCols(d) as a first element
-  Eigen::MatrixXd r = R.array().sqrt();
-  for (int m = 0; m < k; m++) {
-    Eigen::MatrixXd Xo(d, n);
-    for (int i = 0; i < d; i++) {
-      for (int j = 0; j < n; j++) {
-        Xo(i, j) = X(i, j) - mu(i, m);
-      }
-    }
-    Xo = Xo.array() * r.col(m).transpose().array();
-    Sigma.block<1, 1>(0, m) =
-        Xo * Xo.transpose() / nk(m) + Eigen::MatrixXd::Identity(d, d) * (1e-20);
-  }
-
-  gpModel.mu = mu;
-  gpModel.Sigma = Sigma;
-  gpModel.w = w;
-}
-
-vector<int> sort_indexes(Eigen::MatrixXd &v) {
-  // initialize original index locations
-  vector<int> idx(v.cols());
-  iota(idx.begin(), idx.end(), 0);
-
-  // sort indexes based on comparing values in v
-  sort(idx.begin(), idx.end(),
-       [&v](int i1, int i2) { return v(0, i1) < v(0, i2); });
-
-  return idx;
-}
+vector<int> sort_indexes(Eigen::MatrixXd &v);
 
 void CentralizedController::MixGaussEm_gmm() {
   /*Perform EM algorithm for fitting the Gaussian mixture model
@@ -515,20 +369,6 @@ void CentralizedController::gmm_pred_cen(Eigen::VectorXd &pred_h,
   // return ucb_pq;
 }
 
-// void CentralizedController::update_targets(priority_queue< pair<double,int>,
-// vector<pair<double,int>>, less<pair<double,int>>> &ucb_pq)
-// {
-//     for (int i = 0; i < DEFAULT_REQUEST_BUTTER_SIZE; i++)
-//     {
-//         pair<double,int> target_pair = ucb_pq.top();
-//         int target_id = target_pair.second;
-//         ucb_pq.pop();
-
-//         target_positions[i] =
-//         make_pair(All_Xss(target_id,0),All_Xss(target_id,1));
-//     }
-// }
-
 void CentralizedController::sample_cb(const cmuswarm_msgs::Sample &msg) {
   // cout<<"Master listened !!!"<<endl;
   int id = msg.robot_id;
@@ -580,22 +420,6 @@ void CentralizedController::sample_cb(const cmuswarm_msgs::Sample &msg) {
     R.row(len) = R.row(len) / R.row(len).sum();
     label(len) = rand_id;
   }
-
-  // to be updated:
-  // double utility = msg.utility
-
-  // position(0,0) = pos_x;
-  // position(0,1) = pos_y;
-
-  // // for now use GT data
-  //
-
-  // if (!collectedSamples.count(position)) // new sample -> initial_lize R
-  // {
-  //     int rand_id = rand()%this->numBots;
-  //     this->gpModel.R[position] = vector<double>(this->numBots,0.0);
-  //     this->gpModel.R[position][rand_id] = 1.0;
-  // }
 
   this->UpdateCount++;
   if (this->UpdateCount >= this->EMUpdateRate) this->updateFlag = 1;
@@ -678,9 +502,7 @@ void CentralizedController::update_targets() {
       batch_powercellidx(this->rob_positions, this->All_Xss);
   // cout<<" batch powercellidx"<<endl;
 
-  for (int i = 0; i < q.size(); i++) {
-    //    vector<priority_queue< pair<double,int>, vector<pair<double,int>>,
-    //    less<pair<double,int> > > robot_targets;
+  for (int i = 0; i < q.size(); i++) {\
     robot_targets[i] =
         priority_queue<pair<double, int>, vector<pair<double, int>>,
                        less<pair<double, int>>>();
@@ -688,17 +510,7 @@ void CentralizedController::update_targets() {
     Eigen::MatrixXd close_Xss =
         extract_rows(this->All_Xss, q[i]);  // closes points
 
-    for (int j = 0; j < q[i].size(); j++) {
-      // calculate distance
-      // Eigen::MatrixXd ego_position = All_Xss.row(q[i][j]);
-
-      // Eigen::MatrixXd p_dist = pdist2(ego_position, close_Xss);
-      // // multiply by utility
-      // double utility = 0.0;
-      // for (int k = 0; k < q[i].size(); k++)
-      // {
-      //     utility += p_dist(0,k) * this->phi(q[i][k]);
-      // }
+    for (int j = 0; j < q[i].size(); j++) {\
 
       robot_targets[i].push(make_pair(this->phi(q[i][j]), q[i][j]));
     }
@@ -801,18 +613,7 @@ CentralizedController::CentralizedController(const std::string &master_name_in,
       temp(0, 0) = x_pos;
       temp(0, 1) = j;
       this->initialPoints[i].push_back(temp);
-    }
-    // }
-    // else
-    // {
-    //     for (int j = MAP_Y - 1; j > 0; j -= 5)
-    //     {
-    //         Eigen::MatrixXd temp = Eigen::MatrixXd::Zero(1,2);
-    //         temp(0,0) = x_pos;
-    //         temp(0,1) = j;
-    //         this->initialPoints[i].push_back(temp);
-    //     }
-    // }
+    }\
   }
 
   this->target_positions.resize(numBots);
---
+++ src/sampling-core/node/centralized_sampling_node.cpp
@@ -0,0 +1,77 @@
+#include "gp_utils.h"
+#include "sampling_visualization.h"
+#include <ros/ros.h>
+#include <string>
+
+namespace sampling{
+    class CentralizedSamplingNode{
+        public:
+        CentralizedSamplingNode( const ros::NodeHandle& nh, cosnt ros::NodeHandle& rh): nh_(nh), rh_(rh){
+            load_parameter();
+        }
+        
+        bool load_parameter(){
+            if (!rh_.getParam("ground_truth_data_path", ground_truth_data_path_))
+            {
+                ROS_INFO_STREAM("Error! Missing ground truth temperature data!");
+                return false;
+            }
+
+            /// toda load ground truth data
+
+            if (!rh_.getParam("convergence_threshold", convergence_threshold_))
+            {
+                ROS_INFO_STREAM("Error! Missing gaussian process convergence threshold!");
+                return false;
+            }
+
+            if (!rh_.getParam("ugv_max_speed", ugv_max_speed_))
+            {
+                ROS_INFO_STREAM("Error! Missing ugv maximum speed!");
+                return false;
+            }
+
+            if (!rh_.getParam("ugv_goal_channel", ugv_goal_channel_))
+            {
+                ROS_INFO_STREAM("Error! Missing ugv goal channel name!");
+                return false;
+            }
+
+            /// todo subscribe jackal goal channel
+
+            if (!rh_.getParam("uav_max_speed", uav_max_speed_))
+            {
+                ROS_INFO_STREAM("Error! Missing uav maximum speed!");
+                return false;
+            }
+
+            if (!rh_.getParam("uav_goal_channel", uav_goal_channel_))
+            {
+                ROS_INFO_STREAM("Error! Missing uav goal channel name!");
+                return false;
+            }
+
+            /// todo subscribe pelican goal channel
+            
+            return true;
+        }
+
+        private:
+        ros::NodeHandle nh_, rh_;
+        std::string ground_truth_data_path_;
+        GroundTruthData ground_truth_data_;
+        double convergence_threshold_; 
+
+        double ugv_max_speed_;
+        std::string ugv_goal_channel_;
+        double uav_max_speed_;
+        std::string uav_goal_channel_;
+    };
+}
+
+int main()
+{
+    ros::NodeHandle nh, rh("~");
+    return 0;
+}
+
