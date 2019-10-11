3c5d52a05ccf706900859d334ee16e45227a3662
Yang Zhang <youngzh@umich.edu>
2019-10-11 00:48:57 -0700

    melodic support

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+catkin_make --pkg sampling_msgs
 catkin_make
 source devel/setup.bash
 exec bash
--- setup.sh
+++ setup.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
-sudo apt-get install ros-kinetic-octomap ros-kinetic-octomap-msgs ros-kinetic-costmap-2d ros-kinetic-costmap-2d ros-kinetic-grid-map-ros
+sudo apt-get install ros-kinetic-octomap ros-kinetic-octomap-msgs ros-kinetic-costmap-2d ros-kinetic-grid-map-ros ros-kinetic-ros-localization ros-kinetic-move-base*
+sudo apt-get install ros-melodic-octomap ros-melodic-octomap-msgs ros-melodic-costmap-2d ros-melodic-grid-map-ros ros-melodic-ros-localization ros-melodic-move-base*
 git submodule update --init --recursive
-cd src/simulation/swarm-primitive-dp
-mkdir generated_launch
-cd swarm_simulator/src/cmuswarm_swarm/libgp/
-mkdir build
-cd build
-cmake ..
-make
-cd ../../../../../../..
-cd sampling_core/libgp/
+cd src/sampling_core/libgp/
 mkdir build
 cd build
 cmake ..
