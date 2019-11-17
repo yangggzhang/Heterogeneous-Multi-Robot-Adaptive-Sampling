39dacf89e4723a29081457a673d10b8f5aa04887
Yang Zhang <youngzh@umich.edu>
2019-11-17 12:11:50 -0800

    update setup.sh

--- ssh.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install sshpass
-
-echo "alias sshjackal='sshpass -p 'clearpath' ssh administrator@192.168.131.11'" >> ~/.bashrc
-echo "alias sshpelican='sshpass -p 'asctec' ssh asctec@192.168.131.207'" >> ~/.bashrc
-source ~/.bashrc
