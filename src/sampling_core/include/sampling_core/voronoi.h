91ab83f16dcf48e72a76a35da6557fd84fe64a5f
Yang Zhang <youngzh@umich.edu>
2020-02-25 21:53:16 -0500

    add high dimension distance

--- src/sampling_core/include/sampling_core/voronoi.h
+++ src/sampling_core/include/sampling_core/voronoi.h
@@ -9,38 +9,63 @@
 #include <vector>
 
 namespace sampling {
+enum HeterogenitySpace { SPEED, BATTERYLIFE, MOBILITY };
+
 namespace voronoi {
 class Voronoi {
+ private:
+  double L1Distance(const std::vector<double> &lhs,
+                    const std::vector<double> &rhs);
+
+  double L2Distance(const std::vector<double> &lhs,
+                    const std::vector<double> &rhs);
+
+  // Use motion primitive and euclidean distance to calculate high dimentional
+  // distance.
+  inline double ContinuousDistance(const double &motion_primitive,
+                                   const double &euclidean_distance);
+
+  double HeteroDistance(const std::vector<HeterogenitySpace> &hetero_space,
+                        const std::vector<double> &motion_primitive,
+                        const double &euclidean_distance);
+
+  // agent_locations is a N x 2 matrix, where N is the number of agents.
+  // Returnn an m x n distance map, where map(i,j) represents robot_j 's
+  // distance to location_i
+  Eigen::MatrixXd GetDistanceMap(const Eigen::MatrixXd &agent_locations);
+
+  // agent_locations is a N x 2 matrix, where N is the number of agents.
+  bool IsAgentClosest(const std::vector<HeterogenitySpace> &hetero_space,
+                      const std::vector<std::vector<double>> &motion_primitives,
+                      const Eigen::VectorXd &distance_vec, const int &agent_id);
+
  public:
   Voronoi();
 
   // Normal 2D Voronoi map
   Voronoi(const Eigen::MatrixXd &location);
 
-  Voronoi(const Eigen::MatrixXd &location,
+  Voronoi(const Eigen::MatrixXd &location, const int &num_robots,
+          const std::vector<HeterogenitySpace> &hetero_space,
+          const std::vector<double> &scale_factors,
           const std::vector<std::vector<double>> &motion_primitives);
+
+  Eigen::MatrixXd GetVoronoiCell(const Eigen::MatrixXd &agent_locations,
+                                 const int &agent_id);
 
   bool UpdateVoronoiMap(const Eigen::MatrixXd &agent_locations,
                         const Eigen::VectorXd &scale_factor,
                         std::vector<std::vector<int>> &labels,
                         Eigen::MatrixXd &distance_matrix);
 
-  double L1_Distance(const std::vector<double> &lhs,
-                     const std::vector<double> &rhs);
-
-  double L2_Distance(const std::vector<double> &lhs,
-                     const std::vector<double> &rhs);
-
-  // Use motion primitive and euclidean distance to calculate high dimentional
-  // distance.
-  inline double Continuous_Distance(const double &motion_primitive,
-                                    const double &euclidean_distance);
-
   Eigen::MatrixXd GetLocation();
 
  private:
   Eigen::MatrixXd location_;
+  int num_robots_;
+  std::vector<double> scale_factors_;
   std::vector<std::vector<double>> motion_primitives_;
+  std::vector<HeterogenitySpace> hetero_space_;
 };
 }  // namespace voronoi
 }  // namespace sampling
--- src/sampling_core/src/voronoi.cpp
+++ src/sampling_core/src/voronoi.cpp
@@ -10,9 +10,15 @@ Voronoi::Voronoi() {}
 
 Voronoi::Voronoi(const Eigen::MatrixXd &location) : location_(location) {}
 
-Voronoi::Voronoi(const Eigen::MatrixXd &location,
+Voronoi::Voronoi(const Eigen::MatrixXd &location, const int &num_robots,
+                 const std::vector<HeterogenitySpace> &hetero_space,
+                 const std::vector<double> &scale_factors,
                  const std::vector<std::vector<double>> &motion_primitives)
-    : location_(location), motion_primitives_(motion_primitives) {}
+    : location_(location),
+      num_robots_(num_robots),
+      hetero_space_(hetero_space),
+      scale_factors_(scale_factors),
+      motion_primitives_(motion_primitives) {}
 
 bool Voronoi::UpdateVoronoiMap(const Eigen::MatrixXd &agent_locations,
                                const Eigen::VectorXd &scale_factor,
@@ -44,8 +50,8 @@ bool Voronoi::UpdateVoronoiMap(const Eigen::MatrixXd &agent_locations,
 
 Eigen::MatrixXd Voronoi::GetLocation() { return location_; }
 
-double Voronoi::L1_Distance(const std::vector<double> &lhs,
-                            const std::vector<double> &rhs) {
+double Voronoi::L1Distance(const std::vector<double> &lhs,
+                           const std::vector<double> &rhs) {
   assert(lhs.size() == rhs.size());
   double distance = 0.0;
   for (int i = 0; i < lhs.size(); ++i) {
@@ -54,8 +60,8 @@ double Voronoi::L1_Distance(const std::vector<double> &lhs,
   return distance;
 }
 
-double Voronoi::L2_Distance(const std::vector<double> &lhs,
-                            const std::vector<double> &rhs) {
+double Voronoi::L2Distance(const std::vector<double> &lhs,
+                           const std::vector<double> &rhs) {
   assert(lhs.size() == rhs.size());
   double distance = 0.0;
   for (int i = 0; i < lhs.size(); ++i) {
@@ -64,11 +70,91 @@ double Voronoi::L2_Distance(const std::vector<double> &lhs,
   return std::sqrt(distance);
 }
 
-inline double Voronoi::Continuous_Distance(const double &motion_primitive,
-                                           const double &euclidean_distance) {
+inline double Voronoi::ContinuousDistance(const double &motion_primitive,
+                                          const double &euclidean_distance) {
   double px = exp(motion_primitive * euclidean_distance);
   double nx = exp(-motion_primitive * euclidean_distance);
   return (px - nx) / (px + nx);
+}
+
+Eigen::MatrixXd Voronoi::GetDistanceMap(
+    const Eigen::MatrixXd &agent_locations) {
+  assert(agent_locations.rows() == num_robots_);
+  Eigen::MatrixXd distance_map(location_.rows(), num_robots_);
+  double max_distance = 0.0;
+  for (int i = 0; i < num_robots_; ++i) {
+    Eigen::MatrixXd distance = location_;
+    distance.col(0) = distance.col(0).array() - agent_locations(i, 0);
+    distance.col(1) = distance.col(1).array() - agent_locations(i, 1);
+    distance_map.col(i) = distance.rowwise().norm();
+    max_distance = std::max(max_distance, distance_map.col(i).maxCoeff());
+  }
+  distance_map = distance_map / max_distance;
+  return distance_map;
+}
+
+double Voronoi::HeteroDistance(
+    const std::vector<HeterogenitySpace> &hetero_space,
+    const std::vector<double> &motion_primitive,
+    const double &euclidean_distance) {
+  Eigen::VectorXd distance_vec(hetero_space.size());
+  distance_vec(0) = euclidean_distance;
+  for (int i = 0; i < hetero_space.size(); ++i) {
+    switch (hetero_space[i]) {
+      case SPEED: {
+        distance_vec(i + 1) =
+            ContinuousDistance(motion_primitive[i], euclidean_distance);
+        break;
+      }
+      case BATTERYLIFE: {
+        distance_vec(i + 1) =
+            ContinuousDistance(motion_primitive[i], euclidean_distance);
+        break;
+      }
+      case MOBILITY: {
+        distance_vec(i + 1) =
+            ContinuousDistance(motion_primitive[i], euclidean_distance);
+        break;
+      }
+      default:
+        break;
+    }
+  }
+  return distance_vec.norm();
+}
+
+bool Voronoi::IsAgentClosest(
+    const std::vector<HeterogenitySpace> &hetero_space,
+    const std::vector<std::vector<double>> &motion_primitives,
+    const Eigen::VectorXd &distance_vec, const int &agent_id) {
+  assert(distance_vec.size() == num_robots_);
+  const double agent_distance = HeteroDistance(
+      hetero_space, motion_primitives_[agent_id], distance_vec(agent_id));
+  for (int i = 0; i < num_robots_; ++i) {
+    if (i == agent_id)
+      continue;
+    else {
+      double temp_distance =
+          HeteroDistance(hetero_space, motion_primitives_[i], distance_vec(i));
+      if (temp_distance < agent_distance) return false;
+    }
+  }
+  return true;
+}
+
+Eigen::MatrixXd Voronoi::GetVoronoiCell(const Eigen::MatrixXd &agent_locations,
+                                        const int &agent_id) {
+  Eigen::MatrixXd distance_map = GetDistanceMap(agent_locations);
+  Eigen::MatrixXd cell(location_.rows(), location_.cols());
+  int count = 0;
+  for (int i = 0; i < location_.rows(); ++i) {
+    if (IsAgentClosest(hetero_space_, motion_primitives_, distance_map.row(i),
+                       agent_id)) {
+      cell.row(count++) = location_.row(i);
+    }
+  }
+  cell.conservativeResize(count, location_.cols());
+  return cell;
 }
 
 }  // namespace voronoi
