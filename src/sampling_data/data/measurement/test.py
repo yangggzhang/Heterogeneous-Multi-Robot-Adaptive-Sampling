347a68f071038ccebe412209e662965650182121
Yang Zhang <youngzh@umich.edu>
2020-07-10 20:23:34 -0700

    working temperature simulation

--- src/sampling_data/data/measurement/test.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-x = np.loadtxt("wifi_2_routers.txt")
-print(x)
--- src/sampling_measurement/script/measurement_simulation_server.py
+++ src/sampling_measurement/script/measurement_simulation_server.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python
-import sys
-import rospy
-import time
-import serial
-import re
-from collections import deque
-import numpy as np
-from sampling_msg.srv import RequestMeasurement, RequestMeasurementResponse
-
 import itertools
 import numpy as np
-import matplotlib.pyplot as plt
 import rospkg
+import rospy
+from sampling_msgs.srv import RequestMeasurement, RequestMeasurementResponse
+
 
 class MeasurementSimulator(object):
-
     def __init__(self):
-        self.position_x = None
-        self.position_y = None
-        self.measurement = None
-        self.polyfit_order = None
-        self.polyfit_coef = None
-        self.measurement_simulation_server = None
+        rospy.init_node('measurement_simulation_node')
+        measurement_trial = rospy.get_param("~measurement_trial")
+        self.rospack = rospkg.RosPack()
+        self.polyfit_order = rospy.get_param('~poly_order', 5)
+        self.noise_stdev = rospy.get_param('~noise_stdev', 0.5)
+        self.position_x, self.position_y = self.loadposition(measurement_trial)
+        self.measurement = self.loadmeasurement(measurement_trial)
+        self.polyfit_coef = self.polyfit2d(self.position_x, self.position_y, self.measurement, order=self.polyfit_order)
+        self.measurement_simulation_server = rospy.Service('measurement_simulation', RequestMeasurement, self.simulatemeasurement)
+        rospy.spin()
 
     def polyfit2d(self, x, y, z, order=3):
         ncols = (order + 1)**2
@@ -29,7 +25,7 @@ class MeasurementSimulator(object):
         ij = itertools.product(range(order+1), range(order+1))
         for k, (i,j) in enumerate(ij):
             G[:,k] = x**i * y**j
-        m, _, _, _ = np.linalg.lstsq(G, z)
+        m, _, _, _ = np.linalg.lstsq(G, z, rcond=None)
         return m
 
     def polyval2d(self, x, y, m):
@@ -43,33 +39,26 @@ class MeasurementSimulator(object):
     def loadposition(self, measurement_trial):
         position_x = []
         position_y = []
-        position_file = rospack.get_path('sampling_data') + "/data/position/" + measurement_trial + ".txt"
+        position_file = self.rospack.get_path('sampling_data') + "/data/position/" + measurement_trial + ".txt"
         with open(position_file, "r") as filestream:
             for line in filestream:
-                new_x, new_y = currentline = line.split(",")
+                new_x, new_y = line.split(",")
                 position_x.append(float(new_x))
                 position_y.append(float(new_y))
-        return position_x, position_y
+        return np.array(position_x), np.array(position_y)
 
-    def loadmeasurement(self, meansurement_trail):
-        measurement_file = rospack.get_path('sampling_data') + "/data/measurement/" + measurement_trial + ".txt"
+    def loadmeasurement(self, measurement_trail):
+        measurement_file = self.rospack.get_path('sampling_data') + "/data/measurement/" + measurement_trail + ".txt"
         return np.loadtxt(measurement_file)
     
     def simulatemeasurement(self, req):
         simulated_measurement = self.polyval2d(req.position.x, req.position.y, self.polyfit_coef)
+        simulated_measurement += np.random.normal(0, self.noise_stdev)
         return RequestMeasurementResponse(simulated_measurement)
 
     def main(self):
-        rospy.init_node('measurement_simulation_node')
-        measurement_trial = rospy.get_param("~measurement_trial")
-        self.polyfit_order = rospy.get_param('~poly_order', 5)
-        self.position_x, self.position_y = self.loadposition(measurement_trial)
-        self.measurement = self.loadmeasurement(measurement_trial)
-        self.polyfit_coef = self.polyfit2d(self.position_x, self.position_y, self.measurement, order=self.polyfit_order)
-        self.measurement_simulation_server = rospy.Service('measurement_simulation', RequestMeasurement, self.simulatemeasurement)
-        rospy.spin()
+
         return 0
 
 if __name__ == "__main__":
-    temper = Temper()
-    sys.exit(temper.main())
+    measurement_simulation_server = MeasurementSimulator()
