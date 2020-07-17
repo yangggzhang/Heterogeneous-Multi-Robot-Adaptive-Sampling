547f07e8155b1d1b5e351c6dd025d11b9d85e72c
Yang Zhang <yangggzhang@gmail.com>
2020-07-16 23:23:25 -0700

    local stage1

---
+++ src/sampling_partition/include/sampling_partition/weight_voronoi_partion_params.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <ros/ros.h>
+
+#include <string>
+
+namespace sampling {
+namespace partition {
+
+enum Heterogenity { DISTANCE, SPEED, BATTERYLIFE, TRAVERSABILITY };
+
+class WeightedVoronoiPartitionParam {
+ public:
+  WeightedVoronoiPartitionParam();
+
+  bool LoadFromRosParams(ros::NodeHandle &ph);
+
+  //   std::string navigation_frame;
+
+  //   double navigation_height_m;
+
+  //   double navigation_speed_ms;
+
+  //   double takeoff_distance_m;
+};
+}  // namespace partition
+}  // namespace sampling
