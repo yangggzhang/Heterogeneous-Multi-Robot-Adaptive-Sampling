0ecd9c3d593f96c06242c139e6c14826cdd78521
Yang Zhang <yangggzhang@gmail.com>
2020-07-16 23:32:11 -0700

    add util functions for load params

---
+++ src/sampling_utils/include/sampling_utils/utils_impl.h
@@ -0,0 +1,32 @@
+#include "utils.h"
+
+namespace sampling {
+namespace utils {
+
+template <typename T>
+bool GetParam(XmlRpc::XmlRpcValue &YamlNode, const std::string &param_name,
+              T &data) {
+  if (!YamlNode.hasMember(param_name)) {
+    ROS_INFO_STREAM("Missing parameter : " << param_name);
+    return false;
+  } else {
+    data = static_cast<T>(YamlNode[param_name]);
+  }
+  return true;
+}
+
+template <typename T>
+bool GetParam(XmlRpc::XmlRpcValue &YamlNode, const std::string &param_name,
+              std::vector<T> &data) {
+  if (!YamlNode.hasMember(param_name)) {
+    ROS_INFO_STREAM("Missing parameter : " << param_name);
+    return false;
+  }
+  data.reserve(YamlNode[param_name].size());
+  for (int i = 0; i < YamlNode[param_name].size(); ++i) {
+    data.push_back(static_cast<T>(YamlNode[param_name][i]));
+  }
+  return true;
+}
+}  // namespace utils
+}  // namespace sampling
