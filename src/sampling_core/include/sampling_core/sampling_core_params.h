6c5306329262468d88428515da7e7f6aff1ce40c
Yang Zhang <yangggzhang@gmail.com>
2020-07-29 22:09:43 -0700

    robust gp and initialization

--- src/sampling_core/include/sampling_core/sampling_core_params.h
+++ src/sampling_core/include/sampling_core/sampling_core_params.h
@@ -12,8 +12,8 @@ namespace core {
 
 const std::string KDataPackage = "sampling_data";
 const int KModelUpdateFrequencyCount = 1;
-const int KInitSampleSize = 20;
-const double KInitSampleRatio = 0.15;
+const int KInitSampleSize = 50;
+const double KInitSampleRatio = 0.25;
 
 class SamplingCoreParams {
  public:
