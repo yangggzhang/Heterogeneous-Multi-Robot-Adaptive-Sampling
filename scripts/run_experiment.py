fcec84af4540b633fbb0d3409bd245e26ad32d34
yunfei <syfemily@gmail.com>
2020-07-30 01:19:26 -0400

    update script

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -14,6 +14,7 @@ obstacle_id = "obs_1_"
 # obstacle_yaml = rospack.get_path('sampling_agent')+"/config/fake_agent_config.yaml"
 
 
+
 def terminate_process_and_children(p):
     ps_command = subprocess.Popen("ps -o pid --ppid %d --noheaders" % p.pid, shell=True, stdout=subprocess.PIPE)
     ps_output = ps_command.stdout.read()
@@ -24,40 +25,39 @@ def terminate_process_and_children(p):
     p.terminate()
 
 
-
-def call_generate_obstacle():
-	command = "python "+rospack.get_path('sampling_data')+"/data/obstacle_generation.py"
-	command = shlex.split(command)
-	post_proce = subprocess.Popen(command)
-
-
 for i in range(15):
-	rospy.init_node('en_Mapping', anonymous=True)
-	uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-	roslaunch.configure_logging(uuid)
-	call_generate_obstacle()
+	# for scenario in range(1,4):
+	for scenario in range(1,2):
+		rospy.init_node('en_Mapping', anonymous=True)
+		uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+		roslaunch.configure_logging(uuid)
 
-	command = "rosbag record -O "+bag_name+obstacle_id+str(i)+".bag /report /agent_location_channel /sampling_visualization"
-	command = shlex.split(command)
-	rosbag_proc = subprocess.Popen(command)
+		command = "rosbag record -O "+bag_name+"scenario"+str(scenario)+"_"+str(i)+".bag /sampling_performance"
+		command = shlex.split(command)
+		rosbag_proc = subprocess.Popen(command)
 
-	launch2 = roslaunch.parent.ROSLaunchParent(uuid, ["/home/yunfei/heterogeneous-sampling/src/sampling_core/launch/sampling_simulation.launch"])
-	launch2.start()
+		launch = roslaunch.parent.ROSLaunchParent(uuid, 
+		[(rospack.get_path('sampling_gazebo_simulation')+"/launch/two_ugv_one_uav_simulation.launch","scenario:="+str(scenario))])
+		launch.start()
 
+		rospy.sleep(2)
 
-	launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/yunfei/heterogeneous-sampling/src/sampling_agent/launch/multi_fake_agent.launch"])
-	launch.start()
+		launch2 = roslaunch.parent.ROSLaunchParent(uuid, 
+		[(rospack.get_path('sampling_core')+"/launch/heterogeneous_adaptive_sampling.launch", "scenario:="+str(scenario))])
+		launch2.start()
 
-	rospy.loginfo("started")
+		rospy.loginfo("started")
 
-	rospy.sleep(200)
-	# 2 seconds later
-	launch.shutdown()
-	launch2.shutdown()
-	terminate_process_and_children(rosbag_proc)
-	command = "mv "+bag_name+obstacle_id+str(i)+".bag.active "+bag_name+obstacle_id+str(i)+".bag"
-	command = shlex.split(command)
-	post_proce = subprocess.Popen(command)
-	command = "rosbag reindex "+bag_name+obstacle_id+str(i)+".bag"
-	command = shlex.split(command)
-	post_proce = subprocess.Popen(command)
+		rospy.sleep(200)
+		# 2 seconds later
+		launch.shutdown()
+		launch2.shutdown()
+		os.system("killall -9 gzserver")
+
+		terminate_process_and_children(rosbag_proc)
+		command = "mv "+bag_name+"scenario"+str(scenario)+"_"+str(i)+".bag.active "+bag_name+"scenario"+str(scenario)+"_"+str(i)+".bag"
+		command = shlex.split(command)
+		post_proce = subprocess.Popen(command)
+		command = "rosbag reindex "+bag_name+"scenario"+str(scenario)+"_"+str(i)+".bag"
+		command = shlex.split(command)
+		post_proce = subprocess.Popen(command)
