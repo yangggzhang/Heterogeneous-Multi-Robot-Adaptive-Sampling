03137b938b37604bd713deaf10814bd4c2d52bb6
Yang Zhang <yangggzhang@gmail.com>
2020-07-30 21:18:05 -0700

    robust experiment run script

--- src/sampling_agent/node/sampling_agent_node.cpp
+++ src/sampling_agent/node/sampling_agent_node.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char **argv) {
   ros::init(argc, argv, "sampling_agent_node");
   ros::NodeHandle nh, ph("~");
-  ros::Rate r(2);
+  ros::Rate r(3);
 
   std::unique_ptr<sampling::agent::SamplingAgent> agent =
       sampling::agent::SamplingAgent::MakeUniqueFromROS(nh, ph);
@@ -15,9 +15,11 @@ int main(int argc, char **argv) {
     return -1;
   }
 
+  ros::AsyncSpinner spinner(0);
+  spinner.start();
+
   while (ros::ok()) {
     agent->Run();
-    ros::spinOnce();
     r.sleep();
   }
 
